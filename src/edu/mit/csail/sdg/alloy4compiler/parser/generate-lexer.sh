c1c49e607ec22cfe77e5ea8de9db687b4f036998
Vajih Montaghami <vmontagh@uwaterloo.ca>
2012-04-07 10:56:45 +0000

    The version label is correct now.

--- src/edu/mit/csail/sdg/alloy4compiler/parser/generate-lexer.sh
+++ src/edu/mit/csail/sdg/alloy4compiler/parser/generate-lexer.sh
@@ -2,5 +2,5 @@
 
 java -cp /Users/vajih/Documents/workspace2/lib-sdg/jars-external/JFlex.jar JFlex.Main --nobak -d . Alloy.lex
 
-sed -i 's/public java_cup.runtime.Symbol next_token() throws java.io.IOException/public java_cup.runtime.Symbol next_token() throws java.io.IOException, Err/' CompLexer.java
+sed -i -e 's/public java_cup.runtime.Symbol next_token() throws java.io.IOException/public java_cup.runtime.Symbol next_token() throws java.io.IOException, Err/' CompLexer.java
 
