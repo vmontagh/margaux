de9a0c47facde483cd37a35b9a3dadeeb162c27c
Steven Stewart <s26stewa@uwaterloo.ca>
2012-04-29 16:50:04 +0000

    Removing obsolete branch from repository

--- partial-instances-merged-pi-moop/src/edu/mit/csail/sdg/alloy4compiler/parser/generate-lexer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export LIB_SDG='/Users/macbookpro/Documents/AlloyMOOP/lib-sdg/trunk'
-
-java -cp $LIB_SDG/jars-external/JFlex.jar JFlex.Main --nobak -d . Alloy.lex
-
-sed -i -e 's/public java_cup.runtime.Symbol next_token() throws java.io.IOException/public java_cup.runtime.Symbol next_token() throws java.io.IOException, Err/' CompLexer.java
-
--- partial-instances-merged-pi-moop/src/edu/mit/csail/sdg/alloy4compiler/parser/generate-parser.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export LIB_SDG='/Users/macbookpro/Documents/AlloyMOOP/lib-sdg/trunk'
-
-java -cp $LIB_SDG/jars-external/java-cup-11a.jar java_cup.Main \
- -package edu.mit.csail.sdg.alloy4compiler.parser \
- -parser CompParser \
- -progress -time -compact_red \
- -symbols CompSym < Alloy.cup
