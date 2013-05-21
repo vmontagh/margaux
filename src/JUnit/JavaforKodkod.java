4acefdcd2e9f15ad4560a51d024ad124001e2be7
atulan <atulan@atulan-Studio-XPS-1340>
2013-05-21 17:14:47 -0400

    - Junit class

--- src/JUnit/JavaforKodkod.java
+++ src/JUnit/JavaforKodkod.java
@@ -1,5 +1,14 @@
 package JUnit;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class JavaforKodkod {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
