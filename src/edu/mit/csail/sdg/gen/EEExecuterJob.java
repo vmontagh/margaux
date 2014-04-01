4612ecdb5c8be99e3ea01ec9d494b90430cc527b
Vajih Montaghami <vmontagh@uwaterloo.ca>
2014-04-01 09:01:05 -0400

    Just for testing.

--- src/edu/mit/csail/sdg/gen/EEExecuterJob.java
+++ src/edu/mit/csail/sdg/gen/EEExecuterJob.java
@@ -24,7 +24,7 @@ public class EEExecuterJob extends ExecuterJob {
 		
 		Configuration.setProp(Configuration.USING_KODKOD, String.valueOf(true));
 		Configuration.setProp(Configuration.USING_KK_ITR, String.valueOf(false));
-		Configuration.setProp(Configuration.SYMMETRY_OFF, String.valueOf(true));
+		Configuration.setProp(Configuration.SYMMETRY_OFF, String.valueOf(false));
 		Configuration.setProp(Configuration.USING_SYMMETRY, String.valueOf(false));
 
 		
@@ -43,6 +43,6 @@ public class EEExecuterJob extends ExecuterJob {
 		//nsej.callExecuter("models/examples/puzzles/hanoi.als");
 //		nsej = new EEExecuterJob("report.txt");
 //		nsej.callExecuter("models/partial/gen/LL_symmetry_testing.als");
-		nsej.callExecuter("models/partial/gen/symmetry_breaking_short_example.als");
+		nsej.callExecuter("models/partial/gen/abz14/tmp/BST_template_3.als");
 	}
 }
